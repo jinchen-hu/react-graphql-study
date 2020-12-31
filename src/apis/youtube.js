2cf15797d1fad9dbf0c7eeaf20c8c76bbe7fa5f8
Jinchen Hu <jinchenhu.luke@gmail.com>
2020-12-30 20:26:04 -0500

    Update youtube.js

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "AIzaSyB5yMZN2VeHtPWhrpDbywLJFL0fFxXt2t8";
+const KEY = "";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
