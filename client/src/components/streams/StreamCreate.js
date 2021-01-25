1aa940bc3586d17be5b2e6ac3d8a75693727b442
jinchenhu <jinchen.hu@gmail.com>
2021-01-25 10:52:03 -0500

    add client folder

---
+++ client/src/components/streams/StreamCreate.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StreamCreate = () => {
+  return <div>stream create</div>;
+};
+
+export default StreamCreate;
---
+++ client/src/components/streams/StreamEdit.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StreamEdit = () => {
+  return <div>stream edit</div>;
+};
+
+export default StreamEdit;
