b5d8a4d853495ca0f9ea807d673ec89aa694812f
jinchen <jinchenhu.luke@gmail.com>
2021-06-21 17:19:18 -0400

    updated

--- graphql/job-board/server/resolvers.js
+++ graphql/job-board/server/resolvers.js
@@ -1,6 +1,24 @@
-const db = require('./db')
+const db = require("./db");
 
 const Query = {
+  company: (root, { id }) => db.companies.get(id),
+  job: (root, { id }) => db.jobs.get(id),
   jobs: () => db.jobs.list(),
 };
-module.exports = { Query };
+
+const Mutation = {
+  createJob: (root, { input }) => {
+    const id = db.jobs.create(input);
+    return db.jobs.get(id);
+  },
+};
+
+const Company = {
+  jobs: (company) =>
+    db.jobs.list().filter((job) => job.companyId === company.id),
+};
+
+const Job = {
+  company: (job) => db.companies.get(job.companyId),
+};
+module.exports = { Query, Job, Company, Mutation };
